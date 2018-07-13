2fe4bd5a889ae4398ca9996e96a5ed237474da19
deep <deep.com>
2018-07-13 12:29:34 +0800

    项目基础》master

---
+++ vue-bolg/src/components/HomeButton.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="HomeButton">
+		<a href="#/">
+			<button>返回主页</button>
+		</a>
+	</div>
+</template>
+
+<script>
+export default {
+	name:'HomeButton',
+	data (){
+		return {
+			
+		}
+	}
+}
+</script>
+
+<style>
+</style>
---
+++ vue-bolg/src/components/Page1.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="page1">
+		<h1>第一个页面</h1>
+		<h2>{{msg}}</h2>
+		<home-button></home-button>
+		
+		<ul>
+			<li v-for="(n,index) in evenNumbers" :key="index">{{ n }}</li>
+		</ul>
+		
+	</div>
+	
+</template>
+
+<script>
+	
+import HomeButton from './HomeButton';
+
+export default{
+	name: 'Page1',
+	data (){
+		return {
+			msg:'通过路由切换组件',
+			numbers: [ 1, 2, 3, 4, 5 ],
+		}
+	},
+	components: {
+		'home-button':HomeButton
+	},
+	computed: {
+	  evenNumbers: function () {
+	    return this.numbers.filter(function (number) {
+	      return number % 2 === 0
+	    })
+	  }
+	}
+}
+</script>
+
+<style>
+	
+</style>
